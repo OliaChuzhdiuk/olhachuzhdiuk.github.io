0f70162f40cf618eb1c122f624d64e15b414f754
OliaChuzhdiuk <olacuzduk@gmail.com>
2024-04-13 21:15:13 +0300

    add lecture 31

---
+++ exercises/lecture-31/app.js
@@ -0,0 +1,56 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const template = (item) => `
+<h3>${item.title}</h3>
+<div>${item.body}</div>
+<p>Author: <strong><span class="author" data-id="${item.userId}"></stan></strong></p>
+`;
+
+const xhrPromise = (method, url) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject("Something went wrong!");
+    };
+  });
+
+  return promise;
+};
+
+xhrPromise("GET", url)
+  .then((response) => {
+    const posts = JSON.parse(response);
+    let result = "";
+    posts.forEach((item) => {
+      result += template(item);
+    });
+    document.getElementById("blog").innerHTML = result;
+
+    // Повертаємось до сервера для отримання користувачів
+    return xhrPromise("GET", "https://jsonplaceholder.typicode.com/users");
+  })
+  .then((usersResponse) => {
+    const users = JSON.parse(usersResponse);
+    const authorElements = document.querySelectorAll(".author");
+    authorElements.forEach((authorElement) => {
+      const userId = authorElement.getAttribute("data-id");
+      const user = users.find((user) => user.id == userId);
+      if (user) {
+        authorElement.textContent = user.name;
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
