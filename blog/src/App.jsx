eedd672f18790b494e6cb7593acd65f3192385cf
OliaChuzhdiuk <olacuzduk@gmail.com>
2024-04-25 23:20:22 +0300

    addLecture35

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Blog from "./components/Blog";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let post = {
+    id: 1,
+    title: "Programming Algorithm",
+    content: `The word Algorithm means “a process or set of rules to be followed in calculations or other problem-solving operations”. Therefore Algorithm refers to a set of rules/instructions that step-by-step define how a work is to be executed upon in order to get the expected results.`,
+    cover: "/vite.svg",
+    likes: 5,
+  };
 
   return (
     <>
@@ -17,19 +24,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Blog post={post} key={post.id} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
