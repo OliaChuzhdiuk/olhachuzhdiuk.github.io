dead90b9777238d7f3bd141b58298a9987ca436e
OliaChuzhdiuk <olacuzduk@gmail.com>
2024-04-25 23:01:45 +0300

    change

--- project/js/app.js
+++ project/js/app.js
@@ -1,15 +1,20 @@
 var products = document.querySelectorAll(".product");
 
-
 products.forEach(function (product) {
   product.addEventListener("click", function () {
-      var url = product.getAttribute("data-url");
+    var url = product.getAttribute("data-url");
 
-     var img = product.querySelector("img");
-    var newImageUrl = ""; 
+    var img = product.querySelector("img");
+    var newImageUrl = "";
 
     img.setAttribute("src", newImageUrl);
 
     window.location.href = url;
   });
+});
+document.getElementById("shopButton").addEventListener("click", function () {
+  var url = this.getAttribute("data-url");
+  if (url) {
+    window.location.href = url;
+  }
 });
