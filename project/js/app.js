de52ce552c450d95d58ec80df3d851b89ab7c6cd
OliaChuzhdiuk <olacuzduk@gmail.com>
2024-04-25 22:51:58 +0300

    addChange

--- project/js/app.js
+++ project/js/app.js
@@ -1,22 +1,15 @@
-// Отримуємо всі елементи з класом "product"
 var products = document.querySelectorAll(".product");
 
-// Додаємо обробник події для кожного елемента з класом "product"
+
 products.forEach(function (product) {
   product.addEventListener("click", function () {
-    // Отримуємо значення атрибуту "data-url" для кожного продукту
-    var url = product.getAttribute("data-url");
+      var url = product.getAttribute("data-url");
 
-    // Отримуємо зображення продукту, яке потрібно змінити
-    var img = product.querySelector("img");
+     var img = product.querySelector("img");
+    var newImageUrl = ""; 
 
-    // Отримуємо шлях до нового зображення, яке потрібно встановити
-    var newImageUrl = ""; // Заповніть новий шлях до зображення з вашого джерела даних
-
-    // Встановлюємо новий шлях до зображення
     img.setAttribute("src", newImageUrl);
 
-    // Перенаправляємо на сторінку, вказану в атрибуті "data-url"
     window.location.href = url;
   });
 });
