15ddf294b6d1bdf3ccd5c8c41cf5850b362e5a08
OliaChuzhdiuk <olacuzduk@gmail.com>
2024-04-25 22:25:46 +0300

    addLecture34

---
+++ blog/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../../src/App.css";
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark text-light py-4">
+      <div className="container text-center">
+        <p>&copy; 2024 My Shop. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ blog/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import "../../src/App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <ul className="navbar">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
